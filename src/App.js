dc6cbc7e695f8b515a196f9cb36505556767c669
mew27 <terzolopaolo@libero.it>
2022-04-29 17:37:10 +0200

    Modified your card state

--- src/App.js
+++ src/App.js
@@ -5,12 +5,41 @@ import CardAnimation from './CardAnimation'
 import PubSub from 'pubsub-js'
 import React from 'react';
 
+function shuffle(array) {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+
+    return array;
+  }
+
 class App extends React.Component {
 
   state = {turn : 1}
 
   constructor (props) {
     super(props)
+
+    this.deck = []
+
+    let semi = ["Spade", "Denari", "Bastoni", "Coppe"]
+
+    for (const seme of semi) {
+        for (let i = 1; i < 11; i++) {
+            this.deck.push(seme + "_" + i.toString())
+        }
+    }
+    shuffle(this.deck);
   }
 
   componentDidMount() {
@@ -19,13 +48,25 @@ class App extends React.Component {
     })
   }
 
+
+  drawCards(numDraw) {
+      var drawnCards = []
+
+      for(var i = 0; i < numDraw; i++) {
+          drawnCards.push(this.deck.pop())
+      }
+
+      return drawnCards
+  }
+
+
   render() {
     return (
       <>
       <CardAnimation></CardAnimation>
       <div className="TableContainer">
         <div className="Table">
-          <CardHolder></CardHolder>
+          <CardHolder yourCards={this.drawCards(10)} enemyCards={this.drawCards(10)}></CardHolder>
         </div>
       </div>
 
--- src/CardHolder.js
+++ src/CardHolder.js
@@ -3,61 +3,16 @@ import "./CardHolder.css"
 import Card from "./Card"
 import PlayedCards from "./PlayedCards"
 
-
-function shuffle(array) {
-    let currentIndex = array.length,  randomIndex;
-
-    // While there remain elements to shuffle.
-    while (currentIndex != 0) {
-
-      // Pick a remaining element.
-      randomIndex = Math.floor(Math.random() * currentIndex);
-      currentIndex--;
-
-      // And swap it with the current element.
-      [array[currentIndex], array[randomIndex]] = [
-        array[randomIndex], array[currentIndex]];
-    }
-
-    return array;
-  }
-
 class CardHolder extends React.Component {
 
-    state = {played: null}
+    state = {yourCards: this.props.yourCards, enemyCards: this.props.enemyCards}
 
     constructor(props) {
         super(props)
-        this.setPlayed = this.setPlayed.bind(this)
-        this.deck = []
-
-        let semi = ["Spade", "Denari", "Bastoni", "Coppe"]
-
-        for (const seme of semi) {
-            for (let i = 1; i < 11; i++) {
-                this.deck.push(seme + "_" + i.toString())
-            }
-        }
-        shuffle(this.deck);
     }
 
     componentDidMount() {
 
-    }
-
-    drawCards(numDraw) {
-        var drawnCards = []
-
-        for(var i = 0; i < numDraw; i++) {
-            drawnCards.push(this.deck.pop())
-        }
-
-        return drawnCards
-    }
-
-    setPlayed(played) {
-
-        this.setState({played : played})
     }
 
     render() {
@@ -65,13 +20,13 @@ class CardHolder extends React.Component {
         return (
         <>
             <div className="CardHolderContainer" style={{top:"0px", alignItems: "flex-start"}}>
-                {this.drawCards(10).map((val, index) => {return <Card notClickable={true} key={index} cardtype={val} hideCard={true}></Card>})}
+                {this.state.enemyCards.map((val, index) => {return <Card notClickable={true} key={index} cardtype={val} hideCard={true}></Card>})}
             </div>
 
             <PlayedCards></PlayedCards>
 
             <div className="CardHolderContainer" style={{bottom: "0px", alignItems: "flex-end"}}>
-                {this.drawCards(10).map((val, index) => {return <Card key={index} cardtype={val} ></Card>})}
+                {this.state.yourCards.map((val, index) => {return <Card key={index} cardtype={val} ></Card>})}
             </div>
         </>)
     }
